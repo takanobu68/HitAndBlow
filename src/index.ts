6f3de556ff8e22d63015f9e36405d672160ee663
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-20 21:02:40 +0900

    繰り返し遊ぶ機能の追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 const modes = ['normal', 'hard'] as const;
+const nextActions = ['play again', 'exit'] as const;
 
 type Mode = (typeof modes)[number];
+type NextActions = (typeof nextActions)[number];
 
 class GameProcedure {
   private currentGameTitle = 'hit and blow';
@@ -15,7 +17,20 @@ class GameProcedure {
     await this.currentGame.setting();
     await this.currentGame.play();
     this.currentGame.end();
-    this.end();
+
+    const action = await promptSelect<NextActions>(
+      'ゲームを続けますか？',
+      nextActions
+    );
+
+    if (action === 'play again') {
+      await this.play();
+    } else if (action === 'exit') {
+      this.end();
+    } else {
+      const neverValue: never = action;
+      throw new Error(`${neverValue} is an invalid action`);
+    }
   }
 
   private end() {
