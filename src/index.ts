820c615833485f1bff4dea72e27ceabe2d79c56b
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-20 15:29:25 +0900

    settingメソッドの追加

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,24 @@ class HitAndBlow {
   answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   answer: string[] = [];
   tryCount = 0;
+
+  async play() {
+    const inputArr = (
+      await promptInput('[,]区切りで3つの数字を入力してください')
+    ).split(',');
+  }
+
+  setting() {
+    const answerLength = 3;
+
+    while (this.answer.length < answerLength) {
+      const randNum = Math.floor(Math.random() * this.answerSource.length);
+      const selectedItem = this.answerSource[randNum];
+      if (!this.answer.includes(selectedItem)) {
+        this.answer.push(selectedItem);
+      }
+    }
+  }
 }
 
 const printLine = (text: string, breakLine: boolean = true) => {
@@ -18,4 +36,5 @@ const promptInput = async (text: string) => {
 
 (async () => {
   const hitAndBlow = new HitAndBlow();
+  hitAndBlow.setting();
 })();
